4ab0bb32152fd74792337511f8ee7efd6a13facb
Michael Hofer <hoferdev.mi@gmail.com>
2023-05-03 19:18:59 +0200

    First big draft, only small bugfix on bounce button needed

--- README.md
+++ README.md
@@ -2,14 +2,23 @@
 
 This application written with React will show a random color when a button is clicked.
 
+##
+
+red, orange, yellow, green, blue, purple, pink and monochrome are accepted strings for hue
+
+bright, light or dark are accepted strings for luminousity
 Goal:
 A button with the exact text Generate will cause a new color to be generated when it is clicked
 Once a color is generated, update the background color of a div which contains the exact text Generated Color: <background color hex code> (the background color and the hex code must match)
 
 TODOS:
-[ ] - Setup Project
-[ ] -
-[ ] -
-[ ] -
-[ ] -
-[ ] -
+[x] - Setup Project
+[x] - Create Page Layout with Generate button
+[ ] - Features needed:
+[x] - Find library to generate color
+[x] - implement randomcolor npm package
+[x] - ask for hue and luminosity (used choice variant instead of passing strings-error-handling)
+[x] - smooth color transitions
+[x] - use emotion css in js library
+[x] - ask for a size for the color box and generate a box of that size
+[ ] - Make a "screensaver mode" where the color box moves around in a diagonal pattern, bouncing off the screen edges and changing color
--- src/App.js
+++ src/App.js
@@ -1,3 +1,111 @@
 import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
-export default function App() {}
+// using emotion to set css properties for basic style for my box
+const baseBoxStyles = css`
+  transition: background-color 0.5s ease;
+  background-color: #9e9e9e;
+  padding: 60px 20px;
+  border-radius: 10px;
+  width: 400px;
+  height: 400px;
+  font-size: 18px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  box-shadow: 5px 5px 5px;
+`;
+
+// save animation conditional to variable to integrate easier into emotion css
+// outside of the element because of emotion best practices
+// const bounceCondition = css`
+//   animation: ${bounce} 12s linear infinite;
+// `;
+
+export default function App() {
+  // set State variables for background color, text, and hue/brightness select fields
+  const [bgColor, setBgColor] = useState('#ffffff');
+  const [text, setText] = useState(
+    'Generated Color: <background color hex code>',
+  );
+  const [brightness, setBrightness] = useState('random');
+  const [hue, setHue] = useState('');
+  // state variable for generating box
+  const [size, setSize] = useState(400);
+  // state variable defining bouncing state
+  // const [isBouncing, setIsBouncing] = useState(false);
+  // state variable to toggle classes
+  const [classes, setClasses] = useState({ inner: '', outer: '' });
+  // function to handle class button click
+  const handleClick = () => {
+    setClasses({ inner: 'y', outer: 'x' });
+  };
+  // function to update size state variable:
+  const handleChange = (e) => {
+    setSize(e.target.value);
+  };
+
+  // function to set up variables for the generated color and
+  // to set up the text content displaying the same color
+  const changeColor = () => {
+    const newColor = randomColor({
+      // use hue only when not empty
+      hue: hue || undefined,
+      luminosity: brightness,
+    });
+    setBgColor(newColor);
+    setText(`Generated Color: <${newColor}>`);
+  };
+
+  return (
+    <div className="App">
+      <div className={classes.outer}>
+        <div // combine the base css settings with dynamic variables inside the JSX with emotion
+          className={classes.inner}
+          css={css`
+            ${baseBoxStyles};
+            background-color: ${bgColor};
+            width: ${size}px;
+            height: ${size}px;
+          `}
+        >
+          <button onClick={changeColor}>Generate</button>
+          <br />
+          <p>{text}</p>
+          Hue:
+          <select value={hue} onChange={(e) => setHue(e.target.value)}>
+            <option value="">None</option>
+            <option value="red">Red</option>
+            <option value="orange">Orange</option>
+            <option value="yellow">Yellow</option>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+            <option value="purple">Purple</option>
+            <option value="pink">Pink</option>
+            <option value="monochrome">Monochrome</option>
+          </select>
+          <br />
+          Brightness:
+          <select
+            value={brightness}
+            onChange={(e) => setBrightness(e.target.value)}
+          >
+            <option value="random">Random</option>
+            <option value="bright">Bright</option>
+            <option value="dark">Dark</option>
+          </select>
+          <br />
+          Box Size
+          <input type="number" value={size} onChange={handleChange} />
+          <br />
+          <button onClick={handleClick}>Bounce!</button>
+        </div>
+      </div>
+    </div>
+  );
+}
